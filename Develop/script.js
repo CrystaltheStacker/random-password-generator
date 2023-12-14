94df3fef82d8dfab1a6979fd24edf886be0bac9c
Crystal Hikal <crystaluncwebdev@gmail.com>
2023-12-13 22:51:10 -0500

    Final code changes, pending screenshots and some final work on README

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,70 @@
-// Assignment code here
+let characterLength = 8;
+let userSelection = [];
+let choiceArr = [];
 
-// Ref to the HTML "generate" id for my red button
-var generateBtn = document.querySelector("#generate");
+const lowercaseLetters = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+const uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+const singleNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialCharacters = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~",];
 
-function generatePassword() {
-  // Setting up prompts for password criteria.
-  //  Password length 8-128; letters (lower/uppercase), numbers and special characters
-  //  They must choose at least one of the criteria! const started below 
-  //  Voila, password generated and displayed.
+const generateBtn = document.querySelector("#generate");
+
+const writePassword = () => {
+  const correctPrompts = getPrompts();
+  const passwordText = document.querySelector("#password");
+
+  if (correctPrompts) {
+    const newPassword = generatePassword(choiceArr);
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
+};
+
+const generatePassword = (choiceArr) => {
+  let password = "";
+
+  for (let i = 0; i < characterLength; i++) {
+    const randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+  }
+
+  return password;
+};
+
+const getPrompts = () => {
+
+if (confirm("Would you like lowercase letters in your password?")) {
+  choiceArr = choiceArr.concat(lowercaseLetters);
 }
 
-const lowercaseLetters = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
-const uppercaseLetters = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
-const singleNumbers = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
-const specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-
-
-// This is my code for the writePassword function 
-function writePassword() {
-  var password = generatePassword(); // Where's the generatePassword function? It's called but not yet defined.
-  var passwordText = document.querySelector("#password"); // This is telling the computer where to display the final result 
-
-  passwordText.value = password; //This is like saying "the total value is....to this entire function"
-
+if (confirm("Would you like uppercase letters in your password?")) {
+  choiceArr = choiceArr.concat(uppercaseLetters);
 }
 
-// When the user clicks on the red button it will call the writePassword function code
+if (confirm("Would you like numbers in your password?")) {
+  choiceArr = choiceArr.concat(singleNumbers);
+}
+
+if (confirm("Would you like special characters in your password?")) {
+  choiceArr = choiceArr.concat(specialCharacters);
+}
+    characterLength = parseInt(
+      prompt(
+        "How many characters do you want to include in your password? Choose from 8 - 128 characters."
+      )
+    );
+  
+if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert(
+      "Character length is a number from 8 - 128 digits. Please try again."
+    );
+    return false;
+  } else {
+    return true;
+  }
+};
+
+
+
 generateBtn.addEventListener("click", writePassword);
